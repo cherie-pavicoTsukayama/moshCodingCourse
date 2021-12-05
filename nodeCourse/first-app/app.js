1be1857d724895e68151123e0628464048974e83
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2021-12-05 15:41:36 -0800

    review on EventEmitter class

--- nodeCourse/first-app/app.js
+++ nodeCourse/first-app/app.js
@@ -1,5 +1,6 @@
 const os = require('os');
 const fs = require('fs');
+const EventEmitter = require('events') //ths is a class that's why its first letter is capitolized and the word is camel cased.
 
 //OS example
 var totalMemory = os.totalmem();
@@ -17,3 +18,18 @@ fs.readdir('./', function(err, files){
   if(err) console.log('Error ', err);
   else console.log('Files ', files);
 })
+
+
+/*Event Examples. See line 3 where we require node's events method.
+    The order of execution matters when working with the class. You need to
+    Initiate the class first.
+    Then register a listener/turn it on
+    Then raise an event
+*/
+const emitter = new EventEmitter(); // here we are creating an instance of the EventEmitter class.
+// Regisert a listenter
+emitter.on('messageLogged', function(){
+  console.log ('Listener called')
+});
+//Raise an event
+emitter.emit('messageLogged') //emit means makings a nooise or porduce something... signaling that somehting has happend.
