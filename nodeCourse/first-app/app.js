d66b04d46b1791f0c3311d78f005dad96c88942f
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2021-12-05 15:18:19 -0800

    node fs module

--- nodeCourse/first-app/app.js
+++ nodeCourse/first-app/app.js
@@ -1,10 +1,15 @@
 const os = require('os');
+const fs = require('fs');
 
+//OS example
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
-
-//console.log('Total Memory: ' + totalMemory);
 
 
 console.log(`Total Memory: ${totalMemory}`);
 console.log(`Free Memory: ${freeMemory}`);
+
+//fs example
+const files = fs.readdirSync('./');
+
+console.log(`Files:  ${files}`);
