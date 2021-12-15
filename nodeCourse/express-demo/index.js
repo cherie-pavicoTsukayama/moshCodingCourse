d5e46ac373ca18117c418131d1e323ba649ed1cd
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2021-12-15 09:58:26 -0800

    created another get method with the path /api/courses that returns an aray to the local host

--- nodeCourse/express-demo/index.js
+++ nodeCourse/express-demo/index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3]);
+})
+
 app.listen(3000, () => console.log('listening on port 3000'));
