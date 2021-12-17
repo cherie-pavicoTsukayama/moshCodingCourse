e73fd4982d5a235341d8f1773526d74ef55bd9d8
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2021-12-17 09:30:08 -0800

    installed nodemon to global.

--- nodeCourse/express-demo/index.js
+++ nodeCourse/express-demo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hello world!!');
 });
 
 app.get('/api/courses', (req, res) => {
