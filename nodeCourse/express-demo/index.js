b4754b8f6e902ee7c82499af85b2d0897d1e5a41
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2021-12-17 09:59:18 -0800

    created a get method for params.

--- nodeCourse/express-demo/index.js
+++ nodeCourse/express-demo/index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]);
-})
+});
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.get('/api/courses/:id', (req, res) => {
+res.send(req.params.id);
+
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
