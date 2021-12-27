73c47fe669a965a159d116de68abf710bd223b1d
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2021-12-27 10:22:37 -0800

    updated dir name

---
+++ nodeCourse/http-project-vidly/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const Joi = require('joi');
+
+const app = express();
+
+app.use(express.json());
+
+//Create an endpoint to get a list of all genres
+//Create a new genre
+//Update a genre
+//Delete an existing genre
