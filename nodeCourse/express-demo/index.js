1a7135c6098aada3281d6f17c9135a611357b860
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-01-07 09:43:51 -0800

    created a middleware function called logger

--- nodeCourse/express-demo/index.js
+++ nodeCourse/express-demo/index.js
@@ -1,14 +1,12 @@
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  console.log('Logging...');
-  next();
-});
+app.use(logger);
 
 app.use(function(req, res, next) {
   console.log('Authenticating...');
