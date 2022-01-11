688141dc29b5adab20d3eeeea1fe84da505b81a9
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-01-11 09:50:41 -0800

    added express middleware

--- nodeCourse/express-demo/index.js
+++ nodeCourse/express-demo/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('nodeCourse/express-demo/public'));
 
 app.use(logger);
 
