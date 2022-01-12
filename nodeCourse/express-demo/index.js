5b0d566d505bbb9084f8a0145275d9b30e8d7ab2
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-01-12 09:46:49 -0800

    learned about how to set and check my environments using thirdparty middleware morgan

--- nodeCourse/express-demo/index.js
+++ nodeCourse/express-demo/index.js
@@ -6,12 +6,16 @@ const authenticate = require('./authenticate');
 const express = require('express');
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('nodeCourse/express-demo/public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...')
+}
+
 
 app.use(logger);
 
