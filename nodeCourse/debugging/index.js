f2519c1507a56e475fc2a34283505929aec40178
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-05-21 11:13:18 -0700

    reviwed the node debugger

--- nodeCourse/debugging/index.js
+++ nodeCourse/debugging/index.js
@@ -1,11 +1,16 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const morgan = require('morgan');
 const express = require ('express');
 const app = express();
 
-if (app.get('evn') === 'development') {
+if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('Morgan enabled...');
+  startupDebugger('Morgan enabled...');
 }
+
+// Db work...
+dbDebugger('Connected to the database...');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
