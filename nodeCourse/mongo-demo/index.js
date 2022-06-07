fa2969433590f3cdbb01ff5b28fb379a59c961ae
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-06-07 16:42:50 -0700

    created the mongoose schema and a class containing the shcema so that we can create instances of the class and create new mongodb documents in the database collection

--- nodeCourse/mongo-demo/index.js
+++ nodeCourse/mongo-demo/index.js
@@ -2,3 +2,32 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.log('Could not connect to MongoDB...', err));
+
+  //Schemas Types
+    // String
+    // Number
+    // Date
+    // Buffer
+    // Boolean
+    // ObjectID
+    // Array
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+  //Model
+  //using the mongoose method model to create a Class what uses the courseSchema Schema
+  //to create instances with that schema.
+
+const Course = mongoose.model('Coures', courseSchema);
+const course = new Course({
+  name: 'Node.js Course',
+  author: 'Mosh',
+  tags: [ 'node', 'backend' ],
+  isPublished: true
+})
