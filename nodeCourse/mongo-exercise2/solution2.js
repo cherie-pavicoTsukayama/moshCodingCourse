e1da6e07ad16c5bab9feb0d401b5b192294830e4
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-06-16 18:48:40 -0700

    created a new dir and set up a new solution.js to complete exercise 2. Exercise two needed to display documents that were published with a backend tag, sorted by decending price, and only return the name and author.

--- nodeCourse/mongo-exercise2/solution2.js
+++ nodeCourse/mongo-exercise2/solution2.js
@@ -1,35 +1,31 @@
+const { runMain } = require('module');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongo-exercises')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch( err => console.log('Failed to connect to MongoDB...', err));
+  .catch(err => console.log('Failed to connect...', err));
+
 
 const courseSchema = new mongoose.Schema({
+  name: String,
   tags: [ String ],
   date: Date,
-  name: String,
   author: String,
   isPublished: Boolean,
-  price: Number,
-  __v: Number
-})
+  price: Number
+});
 
 const Course = mongoose.model('Courses', courseSchema);
 
 async function getCourses() {
-return await Course
-    .find({
-      isPublished: true,
-      tags: 'backend'
-    })
-    .sort({ name: 1 })
+  return await Course
+    .find({ isPublished: true, tags: 'backend' })
+    .sort({ price: -1 })
     .select({ name: 1, author: 1 })
-};
+}
 
-
-
-
-async function run() {
-  const courses = await getCourses();
-  console.log('Courses', courses);
+async function run(){
+  const courses = await getCourses()
+  console.log('Courses by Decending Price', courses)
+}
 
 run();
