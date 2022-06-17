77d539d359b0b5b861aa92f6d809a05a9294bb64
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-06-16 18:07:48 -0700

    setting up the dir and index to query mongoDB.

---
+++ nodeCourse/mongo-exercise1/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mongo-exercises')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch( err => console.log('Failed to connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+  tags: [ String ],
+  date: { type: date, default: Date.now },
+  name: String,
+  author: String,
+  isPublished: Boolean,
+  price: Number,
+  __v: Number
+})
+
+  async function getCourses(
+const courses = await Course
+    .find()
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 })
+    console.log(courses)
+
+  catch(err) {
+    console.log('Error', err.message);
+  }
+)
