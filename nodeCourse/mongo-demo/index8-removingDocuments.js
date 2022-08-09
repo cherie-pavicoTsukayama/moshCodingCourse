84987418c497944cd36cc0ca066a7dccb42d9a73
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2022-08-09 09:59:43 -0700

    created the removeCourse method

--- nodeCourse/mongo-demo/index8-removingDocuments.js
+++ nodeCourse/mongo-demo/index8-removingDocuments.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch( err => console.log('Could not connect to MongoDB...', err ));
+  .catch(err => console.log('Could not connect to MongoDB...', err));
 
 //Schemas Types
 // String
@@ -13,9 +13,9 @@ mongoose.connect('mongodb://localhost/playground')
 // Array
 
 const courseSchema = new mongoose.Schema({
-  name: String;
-  author: String;
-  tags: [String],
+  name: String,
+  author: String,
+  tags: [ String ],
   date: { type: Date, default: Date.now },
   isPublished: Boolean
 });
@@ -23,11 +23,36 @@ const courseSchema = new mongoose.Schema({
 //Model
 //using the mongoose method model to create a Class what uses the courseSchema Schema
 //to create instances with that schema.
-const Course = mongoose.model('Course', courseSchema);
+
+const Course = mongoose.model('Coures', courseSchema);
+
+//save the Course instance into the database
+async function createCourse() {
+ try {
+  const course = new Course ({
+    name: 'Calligraphy Course',
+    author: 'Cherie',
+    tags: ['writting'],
+    isPublished: true
+  })
+   const result = await course.save();
+   console.log(result);
+}
+  catch(err){
+    console.log('Error:', err.message);
+  }
+}
+
 
 //Removing a course
 async function removeCourse(id) {
-
+  //const result = await Course.deleteOne({ _id: id }); deletes one course.
+  //const result = await Course.deleteMany({ _id: id }); will delete the first one it finds
+  const result = await Course.findByIdAndRemove({ _id: id });
+  console.log('Course deleted:', result);
 }
 
-removeCourse('629fe47b1a2448051f598383')
+
+
+//createCourse();
+removeCourse('62f2924334c72c3ae1820499');
